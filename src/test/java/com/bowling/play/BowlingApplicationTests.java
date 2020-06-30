302299ce8268bea1d0e3faf32213d899fe54e350
Paul Devlin <Paul.Devlin@nihr.ac.uk>
2020-06-30 21:48:19 +0100

    Initial Commit

---
+++ src/test/java/com/bowling/play/BowlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowling.play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
