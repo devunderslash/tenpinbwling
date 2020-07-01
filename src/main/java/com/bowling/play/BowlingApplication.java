bc2df7657998061eee7a6bd76e746a65d5dd2580
Paul Devlin <Paul.Devlin@nihr.ac.uk>
2020-07-02 00:06:01 +0100

    Import run

--- src/main/java/com/bowling/play/BowlingApplication.java
+++ src/main/java/com/bowling/play/BowlingApplication.java
@@ -5,12 +5,21 @@ import com.bowling.play.Importer.Impl.FileReaderServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class BowlingApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(BowlingApplication.class, args);
+		SpringApplication.run(BowlingApplication.class, args);
 		FileReaderService fileReaderService = new FileReaderServiceImpl();
+
+		if(args.length==0){
+			System.err.println("ERROR!! Please run the command with filename, ie:\nmvn compile && mvn exec:java -Dexec.args=\"filename\"");
+			return;
+		}
+
+		Stream<String> lines = fileReaderService.readFile(args[0]);
 
 
 	}
